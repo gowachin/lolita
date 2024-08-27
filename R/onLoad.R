516782333a0cad4fc6b7e1518f519243cba4a71d
gowachin <maxime.jaunatre@yahoo.fr>
2024-08-27 15:23:53 +0200

    win workds

--- R/onLoad.R
+++ R/onLoad.R
@@ -6,6 +6,9 @@
                      file.path(getwd(), ".Rprofile"),
                      file.path(Sys.getenv("HOME"), ".Rprofile"))
     of <- Filter(file.exists, candidates)[1]
+    if(is.na(of)){
+        of <- file.path(Sys.getenv("HOME"), ".Rprofile")
+    }
     if(file.exists(of)){
         rl <- readLines(of)
         if(!any(grepl("lolita", rl))){
